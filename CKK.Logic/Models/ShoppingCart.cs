3c931709f625dec3de95d07f3440e721fd450ea3
Serena Brownson <brownsonserena916@gmail.com>
2022-08-24 17:54:10 -0600

    Get Help With AddProduct_AddProductWhenExists

--- CKK.Logic/Models/ShoppingCart.cs
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -36,38 +36,48 @@ namespace CKK.Logic.Models
             return AddProduct(prod, 1); 
         }
         public ShoppingCartItem AddProduct(Product prod, int quantity) //I think something in here is messing with GetTotal Method.
-        {   if ( quantity >= 1)
+        {   if (quantity >= 1) //checks for valid quantity
             {
-                if (Product1 != null && Product1.GetProduct().GetId() == prod.GetId()) //Example from Instruct
+                if (Product1 != null && Product1.GetProduct().GetId() == prod.GetId()) //Example from Instruct // checks for product 
                 {
-                    Product1.SetQuantity(Product1.GetQuantity() + quantity);
+                   Product1.SetQuantity(Product1.GetQuantity() + quantity);// adds quantity if found
+                    
+                    return Product1;  
                 }
-                else if (Product2 != null && Product2.GetProduct().GetId() == prod.GetId()) 
+                else if (Product2 != null && Product2.GetProduct().GetId() == prod.GetId())
                 {
                     Product2.SetQuantity(Product2.GetQuantity() + quantity);
+                   
+                    return Product2;
                 }
-                else if (Product3 != null && Product3.GetProduct().GetId() == prod.GetId()) 
+                else if (Product3 != null && Product3.GetProduct().GetId() == prod.GetId())
                 {
                     Product3.SetQuantity(Product3.GetQuantity() + quantity);
+                    
+                    return Product3;
                 }
-                 
-                if (Product1 == null)
+
+                else if (Product1 == null)
                 {
                     Product1 = new ShoppingCartItem(prod, quantity);
+
                     return Product1;
                 }
                 else if (Product2 == null)
                 {
-                     Product2 = new ShoppingCartItem(prod, quantity);
+                    Product2 = new ShoppingCartItem(prod, quantity);
+
                     return Product2;
                 }
                 else if (Product3 == null)
                 {
-                   Product3 = new ShoppingCartItem(prod, quantity);
+                    Product3 = new ShoppingCartItem(prod, quantity);
+
                     return Product3;
                 }
+                else return null;
             }
-            return null;
+            else return null;
         }
          
         public ShoppingCartItem RemoveProduct(Product prod, int quantity) // Passing Now
