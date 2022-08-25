92b3f10937a8c2beba531b4f090929a78fdd772a
Serena Brownson <brownsonserena916@gmail.com>
2022-08-24 18:08:04 -0600

    Fully Functional!!

--- CKK.Logic.Tests/UnitTest1.cs
+++ CKK.Logic.Tests/UnitTest1.cs
@@ -42,15 +42,18 @@ using CKK.Logic.Models;
 
             Product prod1 = new Product();
             ShoppingCartItem i = sc.AddProduct(prod1,1);
+            prod1.SetId(1);
             prod1.SetPrice((decimal)15.00);
 
             //tested and okay
             Product prod2 = new Product();
             ShoppingCartItem i2 = sc.AddProduct(prod2, 1);
+            prod2.SetId(2);
             prod2.SetPrice((decimal)10.00);
 
             Product prod3 = new Product();
             ShoppingCartItem i3 = sc.AddProduct(prod3, 1);
+            prod3.SetId(3);
             prod3.SetPrice((decimal)10.00);
             
            
